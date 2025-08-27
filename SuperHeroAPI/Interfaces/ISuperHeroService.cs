b160b90d48f632c033b30c337733021daf0a51fb
daniel-lima-weega <daniel.lima@weega.com.br>
2025-08-27 17:13:48 -0300

    Adicionado services e interface para o controller de superhero

---
+++ SuperHeroAPI/Interfaces/ISuperHeroService.cs
@@ -0,0 +1,12 @@
+﻿using SuperHeroAPI.Entities;
+
+namespace SuperHeroAPI.Interfaces;
+
+public interface ISuperHeroService
+{
+    Task<List<SuperHero>> GetAllSuperHeroesService();
+    Task<SuperHero> GetSuperHeroById(int id);
+    Task<SuperHero> CreateSuperHero(SuperHero newHero);
+    Task<SuperHero> UpdateSuperHeroService(SuperHero updatedHero);
+    Task<bool> DeleteSuperHero(int id);
+}
